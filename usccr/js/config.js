faff91ec8b600175ca2944a656ef2bff10494b65
Bill Dollins <bill@geomusings.com>
2016-08-10 14:42:37 -0400

    updates through June 2016

--- usccr/js/config.js
+++ usccr/js/config.js
@@ -1,11 +1,11 @@
-var global_month = "2/22 - 3/18";
-var global_month_abbr = "2/22 - 3/18";
+var global_month = "5/23 - 6/17";
+var global_month_abbr = "5/23 - 6/17";
 var global_year = "2016";
-var global_geodata_issue = "usccr_issue_2016_03.geojson";
-var global_tabledata_issue = "usccr_issue_2016_03.csv";
-var global_geodata_basis = "usccr_basis_2016_03.geojson";
-var global_tabledata_basis = "usccr_basis_2016_03.csv";
-var global_tablesource = "usccr_table_source_2016_03.csv";
+var global_geodata_issue = "usccr_issue_2016_05.geojson";
+var global_tabledata_issue = "usccr_issue_2016_05.csv";
+var global_geodata_basis = "usccr_basis_2016_05.geojson";
+var global_tabledata_basis = "usccr_basis_2016_05.csv";
+var global_tablesource = "usccr_table_source_2016_05.csv";
 
 $('link[rel="issues"]').attr("href", "data/" + global_geodata_issue);
 $('link[rel="basis"]').attr("href", "data/" + global_geodata_basis);
